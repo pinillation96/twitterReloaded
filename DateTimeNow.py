2f0136dfb9630b275b41879dfbf0a20e4338551e
pinillation96 <44420238+pinillation96@users.noreply.github.com>
2023-06-02 21:55:41 -0600

    testerror

---
+++ DateTimeNow.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+class DateTimeNow:
+    @staticmethod
+    def now():
+        return datetime.now()
--- tests/test_twitter_reloaded.py
+++ tests/test_twitter_reloaded.py
@@ -1,4 +1,4 @@
-import unittest
+""" import unittest
 from datetime import datetime
 from domain.model.user import User
 from domain.model.tweet import Tweet
@@ -45,4 +45,59 @@ class TestTwitterReloaded(unittest.TestCase):
         self.assertEqual(event_dashboard.get_user_activity(), {self.alice.username: 1, self.bob.username: 1})
 
 if __name__ == '__main__':
+    unittest.main()
+ """
+
+import unittest
+from unittest.mock import Mock, patch
+from datetime import datetime
+from application.twitter_reloaded import TwitterReloaded
+from domain.model.user import User
+from domain.service.tweet_factory import TweetFactory
+from infrastructure.event_logger import EventLogger
+
+
+
+
+class TwitterReloadedTests(unittest.TestCase):
+    def setUp(self):
+        self.user = User("TestUser")
+        self.tweet_factory = TweetFactory()
+        self.event_logger = EventLogger()
+        self.twitter_reloaded = TwitterReloaded(self.tweet_factory, self.event_logger)
+
+    @patch('datetime.datetime.now')
+    def test_create_tweet(self, datetime_now):
+        datetime_now.return_value = datetime(2022, 1, 1)
+        tweet, event = self.twitter_reloaded.create_tweet(self.user, "Test content")
+        self.assertEqual(tweet.id, 1)
+        self.assertEqual(tweet.timestamp, datetime(2022, 1, 1))
+        self.assertEqual(tweet.content, "Test content")
+        self.assertEqual(tweet.user, self.user)
+        self.assertEqual(self.twitter_reloaded.tweets[1], tweet)
+
+    @patch('datetime.datetime.now')
+    def test_reply_to_tweet(self, datetime_now):
+        datetime_now.return_value = datetime(2022, 1, 1)
+        tweet, event = self.twitter_reloaded.create_tweet(self.user, "Test content")
+        reply, event = self.twitter_reloaded.reply_to_tweet(self.user, tweet.id, "Test reply")
+        self.assertEqual(reply.content, "Test reply")
+        self.assertEqual(reply.user, self.user)
+        self.assertIn(reply, tweet.replies)
+
+    @patch('datetime.datetime.now')
+    def test_get_recent_tweets(self, datetime_now):
+        datetime_now.side_effect = [
+            datetime(2022, 1, 1),
+            datetime(2022, 1, 2),
+            datetime(2022, 1, 3)
+        ]
+        tweet1, event = self.twitter_reloaded.create_tweet(self.user, "Test content 1")
+        tweet2, event = self.twitter_reloaded.create_tweet(self.user, "Test content 2")
+        tweet3, event = self.twitter_reloaded.create_tweet(self.user, "Test content 3")
+        recent_tweets = self.twitter_reloaded.get_recent_tweets(2)
+        self.assertEqual([tweet.id for tweet in recent_tweets], [tweet.id for tweet in [tweet3, tweet2]])
+
+# Execute the tests
+if __name__ == "__main__":
     unittest.main()
